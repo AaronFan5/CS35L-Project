f9a2bd34ed1335c4fcd9fecae1473b8d0d9a4b1a
Ilias Bariami <ibariami@g.ucla.edu>
2026-05-29 15:14:04 -0700

    fix route crash by moving togglePollStatus to pollController.js

--- controllers/pollController.js
+++ controllers/pollController.js
@@ -76,10 +76,22 @@ async function voteOnPoll(req, res) {
   }
 }
 
+async function togglePollStatus(req, res) {
+  const { pollId } = req.body;
+  try {
+    const result = await pollService.togglePollStatus(pollId, req.user.username);
+    if (result.error) return res.status(result.status).json({ message: result.error });
+    res.json(result.poll);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
 module.exports = {
   getAllPolls,
   getUserVotes,
   createPoll,
   deletePoll,
-  voteOnPoll
+  voteOnPoll,
+  togglePollStatus
 };
